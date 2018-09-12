a9f59278f05e56937a23c69e31e6dc08645250fc
lirenhao <renhao.li@bjyada.com>
2018-09-12 16:56:11 +0800

    日志记录到mongodb

--- src/emitter.js
+++ src/emitter.js
@@ -2,6 +2,22 @@ const events = require('events')
 const emitter = new events.EventEmitter()
 const logger = require('./logger')
 
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
+
+emitter.on('person', async data => {
+    try {
+        const client = await MongoClient.connect(url, {
+            useNewUrlParser: true
+        })
+        const col = client.db('myinfo').collection('person')
+        await col.insertOne(data)
+        client.close()
+    } catch (e) {
+        logger.warn('mongodb data save error', e, JSON.stringify(data));
+    }
+})
+
 emitter.on('person', data => {
     logger.info(JSON.stringify(data));
 })
