5b5fd158231cbdb2167957ce0c74a53b0f34dfa1
lirenhao <renhao.li@bjyada.com>
2018-10-12 12:51:50 +0800

    支持多appid的改造

--- src/test.js
+++ src/test.js
@@ -1,35 +1,40 @@
-const express = require ('express');
-const fetch = require ('node-fetch');
-const jwt = require ('jsonwebtoken');
+const express = require('express');
+const fetch = require('node-fetch');
+const jwt = require('jsonwebtoken');
 
-const app = express ();
+const app = express();
 
 const client = {
   clientId: 'application1',
   clientSecret: 'secret1',
 };
 
-app.get ('/', (req, res) => {
-  fetch ('http://localhost:3001/oauth/token', {
-    method: 'POST',
-    body: 'grant_type=client_credentials',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: 'Basic YXBwbGljYXRpb24xOnNlY3JldDE=',
-    },
-  })
-    .then (res => res.json ())
-    .then (data => {
-      console.log (data);
-      res.redirect (
-        `http://localhost:3001?access_token=${data.access_token}&templateId=temp1&state=1234567890`
-      );
-    });
+app.get('/', (req, res) => {
+  if (req.query.templateId) {
+    fetch('http://localhost:3001/oauth/token', {
+        method: 'POST',
+        body: 'grant_type=client_credentials',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Basic YXBwbGljYXRpb24xOnNlY3JldDE=',
+        },
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        res.redirect(
+          `http://localhost:3001?access_token=${data.access_token}&templateId=${req.query.templateId}&state=1234567890`
+        );
+      });
+  } else {
+    res.send('query not params templateId, ex:http://localhost:3000?templateId=creditcard');
+  }
+
 });
 
-app.get ('/callback', (req, res) => {
+app.get('/callback', (req, res) => {
   const token = req.query.data;
-  res.send (jwt.verify (token, client.clientSecret));
+  res.send(jwt.verify(token, client.clientSecret));
 });
 
-app.listen (3000);
+app.listen(3000);
